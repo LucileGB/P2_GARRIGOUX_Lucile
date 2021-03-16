a32c35a7e1ab21d9c5910a57a5f97310f7708f8e
LucileGB <72474020+LucileGB@users.noreply.github.com>
2021-03-16 17:52:33 +0100

    Update P2_01_GARRIGOUX_Lucile.py

--- P2_01_GARRIGOUX_Lucile.py
+++ P2_01_GARRIGOUX_Lucile.py
@@ -7,39 +7,56 @@ import time
 url = requests.get('http://books.toscrape.com').text
 soup = BeautifulSoup(url, 'lxml')
 
-def scour_category(url_category):
-    url = requests.get(url_category).text
+
+#Scouring a page is separate from scouring a category
+#so we can reuse the variable url_category to go to the next page.
+def scour_page(url_page, url_category):
+    url = requests.get(url_page).text
     soup = BeautifulSoup(url, 'lxml')
     book_list = soup.find_all('div', class_ = 'image_container')
+    
     for book in book_list:
         info = book.find('a')
         link = 'http://books.toscrape.com/catalogue/' + info['href'].replace('../../../', '')
         print(link)
-        
 
-#scour_category('http://books.toscrape.com/catalogue/category/books/travel_2/index.html')
+    has_next = soup.find('li', class_ = 'next')
+    if has_next:
+        next_page = has_next.find('a')
+        np_link = url_category + next_page['href']
+        scour_page(np_link, url_category)
 
+#Scouring a category is just a call to scour_page.
+def scour_category(url_category):
+    url = requests.get(url_category).text
+    soup = BeautifulSoup(url, 'lxml')
+    scour_page(url_category, url_category)
+
+#Prepares a CSV + calls the scour_category for each category.
 def listing_categories():
     url = requests.get('http://books.toscrape.com').text
     soup = BeautifulSoup(url, 'lxml')
     categ_list = soup.find('ul', class_ = 'nav nav-list').find('ul').find_all('li')
     i = 0
+
     for category in categ_list:
         name = categ_list[i].text.strip()
-        rough_link = categ_list[i].find('a')
-        link = rough_link['href']
-        print('http://books.toscrape.com/' + link)
         with open(f'{name}.csv', 'w', newline = '', encoding = 'utf-8') as csvfile:
             fieldname = ['product_page_url', 'universal_ product_code (upc)', 'title',
                          'price_including_tax', 'price_excluding_tax', 'number_available',
                          'product_description', 'category', 'review_rating', 'image_url']
             writer = csv.writer(csvfile)
             writer.writerow(fieldname)
+        raw_link = categ_list[i].find('a')
+        link = raw_link['href'].replace('index.html', '')
+        scour_category('http://books.toscrape.com/' + link)
+        print('http://books.toscrape.com/' + link)
         i = i +1
 #We sleep for 3 seconds, so we don't get flagged as a bot
         time.sleep(3)
 
 listing_categories()
+#scour_category('http://books.toscrape.com/catalogue/category/books/fantasy_19/')
 
 def extract_book(url_book):
     url = 'http://books.toscrape.com/catalogue/the-coming-woman-a-novel-based-on-the-life-of-the-infamous-feminist-victoria-woodhull_993/index.html'
