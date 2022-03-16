d5f293df74160229faa1fa1643024ad92b7cb932
Lucile Garrigoux <lgarrigoux@hotmail.com>
2022-03-16 11:02:36 +0100

    Shortening RM and starting code refactorisation.

--- P2_01_GARRIGOUX_Lucile.py
+++ P2_01_GARRIGOUX_Lucile.py
@@ -6,10 +6,85 @@ from pathlib import Path
 import requests
 import time
 
-url = requests.get("http://books.toscrape.com").text
-soup = BeautifulSoup(url, "lxml")
-cat_name = "To be filled"
+#url = requests.get("http://books.toscrape.com").text
 delay = 0
+
+class Scraper:
+    def __init__(self):
+        self.root_url = "http://books.toscrape.com"
+        self.root_response = requests.get(root_url).text
+        self.categ_folder = Path("categories/")
+
+    def init_scraping(self):
+        soup = BeautifulSoup(self.root_response, "lxml")
+        categ_list = soup.find("ul", class_="nav nav-list").find("ul").find_all("li")
+
+        try:
+            os.mkdir("covers")
+            os.mkdir("categories")
+        except FileExistsError:
+            pass
+
+        for category in categ_list:
+            self.category_set_up(category)
+
+    def category_set_up(self, category):
+        """
+        Initialize a CSV file for the chosen category, then calls
+        scrape_category.
+        """
+        category_name = category.text.strip()
+        raw_link = category.find("a")
+        link = raw_link["href"].replace("index.html", "")
+        csv_path = self.categ_folder / f"{category_name}.csv"
+
+        with open(csv_path, "w", newline="") as csvfile:
+            fieldnames = [
+                "product_page_url",
+                "universal_ product_code (upc)",
+                "title",
+                "price_including_tax",
+                "price_excluding_tax",
+                "number_available",
+                "product_description",
+                "category",
+                "review_rating",
+                "image_url",
+            ]
+            writer = csv.writer(csvfile)
+            writer.writerow(fieldnames)
+
+        scrape_category(f"http://books.toscrape.com/{link}")
+
+
+    def scrape_category(self, url):
+        """Calls scour_page and stores url_category."""
+        response = requests.get(url_category).text
+        soup = BeautifulSoup(response, "lxml")
+        scour_page(url, url)
+
+
+    def scour_page(url_page, url_category):
+        """Calls extract_book for each book link. Calls itself for the next page."""
+        url = requests.get(url_page).text
+        soup = BeautifulSoup(url, "lxml")
+        book_list = soup.find_all("div", class_="image_container")
+        has_next = soup.find("li", class_="next")
+
+        for book in book_list:
+            info = book.find("a")
+            link = "http://books.toscrape.com/catalogue/" + info["href"].replace(
+                "../../../", ""
+            )
+            extract_book(link)
+
+            if delay > 0:
+                time.sleep(delay)
+
+        if has_next:
+            next_page = has_next.find("a")
+            np_link = url_category + next_page["href"]
+            scour_page(np_link, url_category)
 
 
 def extract_book(url_book):
@@ -44,7 +119,7 @@ def extract_book(url_book):
     review_rating = ratings["class"][1]
     img_tag = soup.find("img")
     img_url = img_tag["src"].replace("../..", "")
-    cover = "http://books.toscrape.com" + img_url
+    cover = self.root_url + img_url
 
     download_cover = requests.get(cover)
     cover_folder = Path("covers/")
@@ -69,77 +144,12 @@ def extract_book(url_book):
         writer.writerow(fields)
 
 
-def scour_page(url_page, url_category):
-    """Calls extract_book for each book link. Calls itself for the next page."""
-    url = requests.get(url_page).text
-    soup = BeautifulSoup(url, "lxml")
-    book_list = soup.find_all("div", class_="image_container")
-    has_next = soup.find("li", class_="next")
-
-    for book in book_list:
-        info = book.find("a")
-        link = "http://books.toscrape.com/catalogue/" + info["href"].replace(
-            "../../../", ""
-        )
-        extract_book(link)
-
-        if delay > 0:
-            time.sleep(delay)
-
-    if has_next:
-        next_page = has_next.find("a")
-        np_link = url_category + next_page["href"]
-        scour_page(np_link, url_category)
-
-
-def scour_category(url_category):
-    """Calls scour_page and stores url_category."""
-    url = requests.get(url_category).text
-    soup = BeautifulSoup(url, "lxml")
-    scour_page(url_category, url_category)
-
 
 def main():
     """Main function. Creates cover directory/csv files and calls scour_category."""
     start = int(time.time())
     print(f"Scraping commencé à {time.strftime('%T')}...")
-    try:
-        os.mkdir("covers")
-        os.mkdir("categories")
-    except FileExistsError:
-        pass
 
-    categ_folder = Path("categories/")
-
-    url = requests.get("http://books.toscrape.com").text
-    soup = BeautifulSoup(url, "lxml")
-    categ_list = soup.find("ul", class_="nav nav-list").find("ul").find_all("li")
-    i = 0
-
-    for category in categ_list:
-        global cat_name
-        cat_name = categ_list[i].text.strip()
-        csv_path = categ_folder / f"{cat_name}.csv"
-        with open(csv_path, "w", newline="") as csvfile:
-            fieldname = [
-                "product_page_url",
-                "universal_ product_code (upc)",
-                "title",
-                "price_including_tax",
-                "price_excluding_tax",
-                "number_available",
-                "product_description",
-                "category",
-                "review_rating",
-                "image_url",
-            ]
-            writer = csv.writer(csvfile)
-            writer.writerow(fieldname)
-
-        raw_link = categ_list[i].find("a")
-        link = raw_link["href"].replace("index.html", "")
-        scour_category("http://books.toscrape.com/" + link)
-        i = i + 1
 
     if delay > 0:
         time.sleep(delay)
@@ -167,3 +177,6 @@ if args.delay:
         main()
 else:
     main()
+
+truc = Scraper()
+truc.init_scraping()
